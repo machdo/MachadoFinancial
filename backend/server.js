ee84b3803e1866296bd540be382540cbf4980c09
Vitor Machado <vitor.machado.raimundo@gmail.com>
2026-02-26 22:29:16 -0300

    ajustando cadastro

--- backend/server.js
+++ backend/server.js
@@ -16,12 +16,21 @@ app.get("/", (req, res) => {
 });
 
 // Middleware Auth
-function auth(req, res, next) {
+async function auth(req, res, next) {
   const token = req.headers.authorization?.split(" ")[1];
   if (!token) return res.status(401).json({ error: "No token" });
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await prisma.user.findUnique({
+      where: { id: decoded.id },
+      select: { id: true },
+    });
+
+    if (!user) {
+      return res.status(401).json({ error: "User from token not found" });
+    }
+
     req.userId = decoded.id;
     next();
   } catch {
